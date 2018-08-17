ff1d31533b4bbff5f13d7725961e7c9b0dfdf70d
Jim Rybarski <jim@rybarski.com>
2018-08-17 11:43:51 -0500

    Add support for ASCII tags

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fmt;
 use std::io;
+use std::string;
 
 /// Tiff error kinds.
 #[derive(Debug)]
@@ -46,6 +47,12 @@ impl Error for TiffError {
 impl From<io::Error> for TiffError {
     fn from(err: io::Error) -> TiffError {
         TiffError::IoError(err)
+    }
+}
+
+impl From<string::FromUtf8Error> for TiffError {
+    fn from(err: string::FromUtf8Error) -> TiffError {
+        TiffError::FormatError(String::from("Image contains invalid tag."))
     }
 }
 
