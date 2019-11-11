686e9b264869ceefeb36409b014ae2b05e7a416f
Onno Kammeraad <onno.kammeraad@technolution.nl>
2019-11-11 16:15:01 +0100

    added a missing offset increase while adding padding

--- src/encoder/writer.rs
+++ src/encoder/writer.rs
@@ -10,6 +10,7 @@ impl TiffByteOrder for LittleEndian {
     fn write_header<W: Write>(writer: &mut TiffWriter<W>) -> TiffResult<()> {
         writer.writer.write_u16::<LittleEndian>(0x4949)?;
         writer.writer.write_u16::<LittleEndian>(42)?;
+        writer.offset += 4;
 
         Ok(())
     }
@@ -19,6 +20,7 @@ impl TiffByteOrder for BigEndian {
     fn write_header<W: Write>(writer: &mut TiffWriter<W>) -> TiffResult<()> {
         writer.writer.write_u16::<BigEndian>(0x4d4d)?;
         writer.writer.write_u16::<BigEndian>(42)?;
+        writer.offset += 4;
 
         Ok(())
     }
@@ -26,67 +28,77 @@ impl TiffByteOrder for BigEndian {
 
 pub struct TiffWriter<W> {
     writer: W,
+    offset: u64,
 }
 
 impl<W: Write> TiffWriter<W> {
     pub fn new(writer: W) -> Self {
-        Self { writer }
+        Self { writer, offset: 0 }
+    }
+
+    pub fn offset(&self) -> u64 {
+        self.offset
     }
 
     pub fn write_bytes(&mut self, bytes: &[u8]) -> Result<(), io::Error> {
         self.writer.write_all(bytes)?;
+        self.offset += bytes.len() as u64;
         Ok(())
     }
 
     pub fn write_u8(&mut self, n: u8) -> Result<(), io::Error> {
         self.writer.write_u8(n)?;
+        self.offset += 1;
         Ok(())
     }
 
     pub fn write_i8(&mut self, n: i8) -> Result<(), io::Error> {
         self.writer.write_i8(n)?;
+        self.offset += 1;
         Ok(())
     }
 
     pub fn write_u16(&mut self, n: u16) -> Result<(), io::Error> {
         self.writer.write_u16::<NativeEndian>(n)?;
+        self.offset += 2;
 
         Ok(())
     }
 
     pub fn write_i16(&mut self, n: i16) -> Result<(), io::Error> {
         self.writer.write_i16::<NativeEndian>(n)?;
+        self.offset += 2;
 
         Ok(())
     }
 
     pub fn write_u32(&mut self, n: u32) -> Result<(), io::Error> {
         self.writer.write_u32::<NativeEndian>(n)?;
+        self.offset += 4;
 
         Ok(())
     }
 
     pub fn write_i32(&mut self, n: i32) -> Result<(), io::Error> {
         self.writer.write_i32::<NativeEndian>(n)?;
+        self.offset += 4;
 
         Ok(())
     }
 
     pub fn write_u64(&mut self, n: u64) -> Result<(), io::Error> {
         self.writer.write_u64::<NativeEndian>(n)?;
+        self.offset += 8;
 
         Ok(())
     }
 
-}
-
-impl<W: Write + Seek> TiffWriter<W> {
     pub fn pad_word_boundary(&mut self) -> Result<(), io::Error> {
-        let offset = self.offset();
-        if offset % 4 != 0 {
+        if self.offset % 4 != 0 {
             let padding = [0, 0, 0];
-            let padd_len = 4 - (offset % 4);
+            let padd_len = 4 - (self.offset % 4);
             self.writer.write_all(&padding[..padd_len as usize])?;
+            self.offset += padd_len;
         }
 
         Ok(())
@@ -95,11 +107,8 @@ impl<W: Write + Seek> TiffWriter<W> {
 
 impl<W: Seek> TiffWriter<W> {
     pub fn goto_offset(&mut self, offset: u64) -> Result<(), io::Error> {
+        self.offset = offset;
         self.writer.seek(SeekFrom::Start(offset as u64))?;
         Ok(())
-    }
-
-    pub fn offset(&mut self) -> u64 {
-        self.writer.seek(SeekFrom::Current(0)).unwrap()
     }
 }
