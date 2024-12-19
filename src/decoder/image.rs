663c19c4560f09621966720bbb9288e04ade0193
Stefan Schake <sschake@quantum-systems.com>
2024-12-19 19:01:42 +0100

    Support ZSTD compression method for reading
    ZSTD is generally recognized as superior to LZW in size and
    speed, particularly for decompression.
    We use the self-assigned tag first defined by libtiff and
    also used by GDAL and others.

--- src/decoder/image.rs
+++ src/decoder/image.rs
@@ -378,7 +378,10 @@ impl Image {
             CompressionMethod::None => Box::new(reader),
             CompressionMethod::LZW => {
                 Box::new(LZWReader::new(reader, usize::try_from(compressed_length)?))
-            }
+            },
+            CompressionMethod::ZSTD => {
+                Box::new(zstd::Decoder::new(reader)?)
+            },
             CompressionMethod::PackBits => Box::new(PackBitsReader::new(reader, compressed_length)),
             CompressionMethod::Deflate | CompressionMethod::OldDeflate => {
                 Box::new(DeflateReader::new(reader))
